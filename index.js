92d9f8d0fd9a5fccb2ba17dfeb702aa4f0f21367
Per Guth <mail@perguth.de>
2022-12-29 04:37:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ class YggdrasilProtectedGroup {
       }
     })
 
-    socket.write(JSON.stringify({ mtime: this.mtime.toString() }))
+    socket.write(JSON.rt.stringify({ mtime: this.mtime.toString() }))
   }
 
   watch () {
