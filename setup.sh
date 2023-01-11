b085d055f1cda84c1c0e0498c7e6bd36ad4f893f
Per Guth <mail@perguth.de>
2023-01-11 14:16:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo rm -rf /opt/$NAME
 cd /opt
 sudo git clone https://github.com/perguth/$NAME.git
 cd $NAME
-sudo npm i --production
+sudo npm i --omit=dev
 
 # Setup service
 sudo cp $NAME.service /etc/systemd/system
