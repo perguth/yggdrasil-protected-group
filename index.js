3e677c9ea95dcff67935ef62373eb37d0b55cf45
Per Guth <mail@perguth.de>
2023-01-11 14:35:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -127,8 +127,20 @@ class YggdrasilProtectedGroup {
       fs.utimesSync(this.path.ypg, date, date)
     }
   }
+  
+  checkVersion () {
+    const version = child.execSync('yggdrasil -version').toString().match(/(\d).(\d).(\d)/)
+    if (version[1] === 0 && (version[2] < 4 || version[2] < 7)) {
+      return false
+    }
+    return true
+  }
 
   async start () {
+    if (!this.checkVersion) {
+      console.error('Yggdrasil is too old (< 0.4.7)')
+      process.exit(1)
+    }
     this.prepare()
 
     this.mtime = fs.statSync(this.path.ypg).mtime
