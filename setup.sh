b06316a737fa9d72a9e8c3b6543ebab2090291f8
Per Guth <mail@perguth.de>
2023-02-09 14:53:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,30 @@
 
 NAME=yggdrasil-protected-group
 
+if ! which node > /dev/null; then
+  echo Install NodeJS first:
+  echo https://nodejs.org/en/download/package-manager/
+  exit 1
+fi
+
+if ! sudo ufw status verbose | grep -q 'Status: inactive'; then
+    echo UFW already enabled and configured! Please reset it first:
+    echo \$ sudo ufw reset
+    exit 1
+  fi
+fi
+
+# Fix Yggdrasil interface name
+sudo sed -i 's/IfName: auto/IfName: ygg0/g' /etc/yggdrasil.conf
+sudo service yggdrasil restart
+
+# Install UFW
+sudo apt install -y ufw
+# Change from whilelist mode to blacklist mode
+sudo ufw default allow
+# Generally block incoming requests via Yggdrasil
+sudo ufw deny in on ygg0
+
 # Clone repository
 sudo rm -rf /opt/$NAME
 cd /opt
