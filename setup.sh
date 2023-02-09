73783a9d9a55c807120b713d13e9874e0f666772
Per Guth <mail@perguth.de>
2023-02-09 18:35:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,24 +10,18 @@ fi
 
 if sudo which ufw > /dev/null; then
   if ! sudo ufw status | grep -q 'Status: inactive'; then
-    echo UFW already enabled and configured! Please reset it first:
+    echo UFW already enabled! Please reset it first:
     echo $ sudo ufw reset
     exit 1
   fi
 fi
 
-# Stop service
-sudo service $NAME stop > /dev/null 2>&1
-
-# Reset swarm state
-SWARM=/etc/opt/$NAME/swarm.json
-if test -f $SWARM; then
-  sudo rm $SWARM
-fi
-
 # Fix Yggdrasil interface name
 sudo sed -i 's/IfName: auto/IfName: ygg0/g' /etc/yggdrasil.conf
 sudo service yggdrasil restart
+
+# Stop service
+sudo service $NAME stop > /dev/null 2>&1
 
 # Install UFW
 sudo apt install -y ufw
@@ -35,6 +29,12 @@ sudo apt install -y ufw
 sudo ufw default allow
 # Generally block incoming requests via Yggdrasil
 sudo ufw deny in on ygg0
+
+# Reset swarm state
+SWARM=/etc/opt/$NAME/swarm.json
+if test -f $SWARM; then
+  sudo rm $SWARM
+fi
 
 # Clone repository
 sudo rm -rf /opt/$NAME
